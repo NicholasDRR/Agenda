0a3286e507f82bd0860060ebf411dc920e3f5c96
Nicholas Ribeiro <90071073+NicholasDRR@users.noreply.github.com>
2022-08-24 16:36:50 -0300

    Criando accounts

---
+++ accounts/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.contrib import messages
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return render(request, 'accounts/logout.html')
+
+
+def register(request):
+    print(request.POST)
+    return render(request, 'accounts/register.html')
+
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
--- agenda/settings.py
+++ agenda/settings.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'contatos.apps.ContatosConfig',
+    'accounts.apps.AccountsConfig',
 
 ]
 
