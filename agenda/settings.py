7b3d3ba6699eb9f46031c0bd55663e8155ec787d
Nicholas Ribeiro <90071073+NicholasDRR@users.noreply.github.com>
2022-08-31 02:09:11 -0300

    Adicionando statics

--- agenda/settings.py
+++ agenda/settings.py
@@ -108,6 +108,8 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'media'
 
 MEDIA_URL = 'media/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
